5ff10918d21981137e0c67d8ed6e51d35199f49c
Robey Pointer <robey@twitter.com>
2012-04-06 10:27:32 -0700

    'release commit for com.twitter:sbt-package-dist:1.0.3-SNAPSHOT'

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,7 @@ object StandardProjectPlugin extends Build {
   ).settings(
     organization := "com.twitter",
     name := "sbt-package-dist",
-    version := "1.0.2",
+    version := "1.0.3-SNAPSHOT",
     SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public"),
     sbtPlugin := true,
     libraryDependencies ++= Seq (
